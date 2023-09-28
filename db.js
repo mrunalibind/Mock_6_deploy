2799ca0ec49a20c15d97f30932c726beda6c3a6e
Mrunali Bind <112799850+mrunalibind@users.noreply.github.com>
2023-09-28 18:58:06 +0530

    done

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 let mongoose=require("mongoose");
-require("dotenv").config();
-let connection=mongoose.connect(process.env.mongoURL);
+// require("dotenv").config();
+let connection=mongoose.connect("mongodb+srv://mrunali:mrunalibind@cluster0.tsxywrf.mongodb.net/Mock6?retryWrites=true&w=majority");
 
 module.exports={connection};
